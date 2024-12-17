0c2ed902f3ca56cd58916c393b77beab2bd044d9
virbaba <virendersinghdev1@gmail.com>
2024-12-17 15:45:23 +0530

    update login

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 import GoogleLogin from "../GoogleLogin";
 import { google_client_id } from "../../utils/GoogleOAuthCredentials.js";
-import axios from 'axios';
+import axios from "axios";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -25,7 +25,6 @@ const Login = () => {
   // Handle form submission
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Log form data for now (or send it to your backend)
     const response = await axios.post(
       "http://localhost:8000/api/v1/user/login",
       {
@@ -37,7 +36,6 @@ const Login = () => {
       email: "",
       password: "",
     });
-    // Navigate based on user role
     const userRole = response.user.role;
     if (userRole.includes("student")) navigate("/");
     else if (userRole.includes("recruiter")) navigate("/post-job");
@@ -45,25 +43,25 @@ const Login = () => {
   };
 
   return (
-    <div className="flex flex-col md:flex-row h-screen">
-      {/* Left Section - Background Image and Content */}
-      <div className="w-full md:w-1/3 flex items-center justify-center bg-gradient-to-l from-white to-blue-100">
-        <form className="w-4/5 space-y-4" onSubmit={handleSubmit}>
+    <div className="flex flex-col-reverse md:flex-row h-screen">
+      {/* Left Section - Form */}
+      <div className="w-full md:w-1/2 lg:w-1/3 flex items-center justify-center bg-gradient-to-l from-white to-blue-100 p-6">
+        <form className="w-full max-w-md space-y-4" onSubmit={handleSubmit}>
           <h1 className="text-3xl font-bold text-center">
             Great<span className="text-blue-700">Hire</span>
           </h1>
-          <h1 className="text-4xl font-bold text-center">Login</h1>
-          <h1 className="text-md font-semibold text-gray-500 text-center">
+          <h1 className="text-2xl md:text-4xl font-bold text-center">Login</h1>
+          <p className="text-sm md:text-md font-semibold text-gray-500 text-center">
             Find the job made for you!
-          </h1>
-          {/* Google login Button */}
+          </p>
+          {/* Google Login Button */}
           <GoogleOAuthProvider clientId={google_client_id}>
             <GoogleLogin text="Login" role="student" />
           </GoogleOAuthProvider>
-          <h1 className="text-sm font-semibold text-gray-400 text-center">
+          <p className="text-sm font-semibold text-gray-400 text-center">
             ---- or Login with email ----
-          </h1>
-          <div className="flex flex-col space-y-2">
+          </p>
+          <div className="flex flex-col space-y-4">
             <label className="font-bold">Email</label>
             <input
               type="email"
@@ -90,28 +88,28 @@ const Login = () => {
             Login
           </button>
           <p className="text-center text-sm text-gray-500">
-            New at GreatHire. Welcome!{" "}
-            <a href="/login" className="text-blue-500 hover:underline">
+            New at GreatHire?{" "}
+            <a href="" className="text-blue-500 hover:underline">
               Signup
             </a>
           </p>
         </form>
       </div>
 
-      {/* Right Section - Form */}
-      <div className="relative w-full md:w-2/3 h-1/2 md:h-full">
+      {/* Right Section - Background Image */}
+      <div className="relative w-full md:w-1/2 lg:w-2/3 h-1/2 md:h-full">
         {/* Background Image */}
         <img
           src={img2}
-          alt="Image 1"
+          alt="Background"
           className="w-full h-full object-cover opacity-80"
         />
         {/* Centered Content */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center text-center space-y-4">
-          <h1 className="font-bold text-3xl md:text-4xl text-gray-600 ">
+        <div className="absolute inset-0 flex flex-col items-center justify-center text-center space-y-4 p-4 z-50">
+          <h1 className="font-bold text-2xl md:text-4xl text-gray-600">
             Find the job made for you.
           </h1>
-          <p className="font-medium text-gray-600 text-md md:text-lg w-4/5 md:w-3/5">
+          <p className="font-medium text-gray-600 text-sm md:text-lg max-w-[90%] md:max-w-[70%]">
             Browse over 150K jobs at top companies.
           </p>
         </div>
