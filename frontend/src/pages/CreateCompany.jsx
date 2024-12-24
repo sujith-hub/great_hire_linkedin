d84a89c2483b73196e60290554eef209b889358c
Mohd Nazir <mohdnazir1007@gmail.com>
2024-12-24 12:20:58 +0530

    worked on update profile cancle button

--- frontend/src/pages/CreateCompany.jsx
+++ frontend/src/pages/CreateCompany.jsx
@@ -66,9 +66,9 @@ const CreateCompany = () => {
   return (
     <div>
       <Navbar />
-      <div className="max-w-3xl mx-auto p-6 bg-white rounded-lg shadow-lg">
+      <div className="max-w-6xl mx-auto p-6 bg-white rounded-lg shadow-lg">
         <h1 className="text-2xl font-bold mb-6 text-center text-blue-600">
-          Company Details Form
+          <u>Company Details Form</u>
         </h1>
         <form onSubmit={handleSubmit} className="space-y-6">
           {/* Section 1: Company Information */}
@@ -294,8 +294,8 @@ const CreateCompany = () => {
                 </label>
                 <input
                   type="text"
-                  name="phoneNumber"
-                  value={formData.recruiterPosition}
+                  name="recruiterPhone"
+                  value={formData.recruiterPhone}
                   onChange={handleChange}
                   className="w-full p-2 border rounded focus:outline-blue-500"
                   required
@@ -317,7 +317,7 @@ const CreateCompany = () => {
             type="submit"
             className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600"
           >
-            Submit Form
+            Submit
           </button>
         </form>
       </div>
