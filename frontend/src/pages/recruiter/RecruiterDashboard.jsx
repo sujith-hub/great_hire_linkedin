fc0ce7bb36047a2ab88b349b02b58da58f57db77
virbaba <virendersinghdev1@gmail.com>
2024-12-28 15:25:42 +0530

    update recruiter dashboard

--- frontend/src/pages/recruiter/RecruiterDashboard.jsx
+++ frontend/src/pages/recruiter/RecruiterDashboard.jsx
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-import React from 'react';
-import { useSelector } from 'react-redux';
-=======
 import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
->>>>>>> 16c1c8fc7eb772a1a81953b2d1135008252c217f
 import Navbar from '@/components/shared/Navbar';
 import Footer from '@/components/shared/Footer';
 import { COMPANY_API_END_POINT } from '@/utils/ApiEndPoint';
@@ -13,29 +8,6 @@ import { addCompany } from '@/redux/companySlice';
 
 const RecruiterDashboard = () => {
   const { user } = useSelector((state) => state.auth);
-<<<<<<< HEAD
-
-  return (
-    <div className="flex flex-col min-h-screen">
-      {/* Navbar at the top */}
-      <Navbar />
-
-      {/* Main Content */}
-      <div className="flex-grow p-4">
-        {user.isVerify === 1
-          ? "Dashboard"
-          : user.isVerify === -1
-          ? "Your organisation denied your verification. Cannot create company, job."
-          : "Still, you are not verified by your company. Great Hire sent a link to your organisation to verify you."}
-      </div>
-
-      {/* Footer at the bottom */}
-      <Footer className="mt-auto" />
-    </div>
-  );
-};
-
-=======
   const { company } = useSelector((state) => state.company);
   const dispatch = useDispatch();
   const [loading, setLoading] = useState(false);
@@ -83,5 +55,4 @@ const RecruiterDashboard = () => {
   );
 };
 
->>>>>>> 16c1c8fc7eb772a1a81953b2d1135008252c217f
 export default RecruiterDashboard;
