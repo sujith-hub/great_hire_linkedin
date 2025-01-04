7a5a34b1c2cf858cd32597677666e8fd757cc266
virbaba <virendersinghdev1@gmail.com>
2025-01-04 18:07:10 +0530

    code pulling

--- frontend/src/pages/recruiter/postJob/PostJob.jsx
+++ frontend/src/pages/recruiter/postJob/PostJob.jsx
@@ -1,22 +1,3 @@
-// eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWNydWl0ZXJJZCI6IjY3NmE2NTU5MzNkOTFhZGM4NTE0YTczZiIsImNvbXBhbnlJZCI6IjY3NmE4MDZjYjQ3ZmFlNTYyZmUxYTA2NCIsImlhdCI6MTczNTAzMjk0MCwiZXhwIjoxNzM1MTE5MzQwfQ.QpxiOtNH7eOU_gXdZK9MCgEcibnUzObX-iJeFuuqyIE
-// import React from "react";
-// import { useSelector } from "react-redux";
-
-// const { user } = useSelector((state) => state.auth);
-// return (
-//   <>
-//     {user.isVerify === 1 ? (
-//       <>
-//         <p>Post job form</p>
-//       </>
-//     ) : (
-//       <>
-//         <p>You are not verified </p>
-//       </>
-//     )}
-//   </>
-// );
-
 import React, { useState, useRef } from "react";
 import { ProgressBar } from "react-step-progress-bar";
 import { BiArrowBack } from "react-icons/bi";
