ac304689796965033660ad30195f3ae0c1ca5a06
virbaba <virendersinghdev1@gmail.com>
2025-02-08 10:44:58 +0530

    update ui of admin dashobard

--- BackEnd/controllers/admin/statistic.controller.js
+++ BackEnd/controllers/admin/statistic.controller.js
@@ -112,3 +112,4 @@ export const getApplicationsDataByYear = async (req, res) => {
     });
   }
 };
+
--- frontend/src/context/JobDetailsContext.jsx
+++ frontend/src/context/JobDetailsContext.jsx
@@ -185,7 +185,6 @@ export const JobDetailsProvider = ({ children }) => {
         addApplicationToJob,
         getSaveJobs,
         saveJobsList,
-        updateSaveJobs,
         error,
       }}
     >
