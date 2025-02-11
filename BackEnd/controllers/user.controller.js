9c68b09f6e669ba582ae271a9bdcc5c3a9552690
virbaba <virendersinghdev1@gmail.com>
2025-02-11 17:59:18 +0530

    update profile correctly

--- BackEnd/controllers/user.controller.js
+++ BackEnd/controllers/user.controller.js
@@ -347,6 +347,7 @@ export const updateProfile = async (req, res) => {
 
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
+      console.log(errors);
       return res.status(400).json({ errors: errors.array() });
     }
 
