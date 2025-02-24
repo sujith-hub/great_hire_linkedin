7ad8b137b2caf67c4f20adf7ff9969f7eb40c6b5
virbaba <virendersinghdev1@gmail.com>
2025-02-24 10:02:24 +0530

    change ui of Jobs.jsx

--- frontend/src/pages/job/Jobs.jsx
+++ frontend/src/pages/job/Jobs.jsx
@@ -73,7 +73,7 @@ const Jobs = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
-      <div className="flex-grow w-full mx-auto bg-gradient-to-r from-gray-100 via-blue-100 to-gray-100">
+      <div className="flex-grow w-full mx-auto bg-gray-100">
         <div className="w-full px-4 py-4">
         <FilterCard onSearch={handleSearch} resetFilters={resetFilters} />
 
