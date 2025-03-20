67e2fbe04601c201e042099ceddd98fbd6df7fd4
amanparida <amanparida7796@gmail.com>
2025-03-20 12:49:38 +0530

    comment out the 404 line in PageNotFound.jsx

--- frontend/src/pages/PageNotFound.jsx
+++ frontend/src/pages/PageNotFound.jsx
@@ -23,15 +23,15 @@ const PageNotFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       {/* Display 404 error message */}
-      <h1 className="text-6xl font-bold text-red-500">404</h1>
-      <p className="text-xl text-gray-700 mt-4">
+      {/* <h1 className="text-6xl font-bold text-red-500">404</h1> */}
+      <p className="text-xl text-gray-900 mt-4 text-center">
         Oops! The page you are looking for does not exist.
       </p>
 
       {/* Button to navigate back to the homepage */}
       <button
         onClick={handleGoBack}
-        className="mt-6 px-6 py-2 text-white bg-blue-500 hover:bg-blue-600 rounded-md transition-all duration-200"
+        className="mt-6 px-6 py-2 text-white bg-blue-700 hover:bg-blue-800 rounded-md transition-all duration-200"
       >
         Go Back Home
       </button>
